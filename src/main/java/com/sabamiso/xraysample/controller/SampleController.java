d92d9c204b3c4ebda59902569c6058ed4993a04b
adachishohei <dankan.0101@gmail.com>
2021-10-20 01:34:23 +0900

    3秒待つように変更

--- src/main/java/com/sabamiso/xraysample/controller/SampleController.java
+++ src/main/java/com/sabamiso/xraysample/controller/SampleController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
     @GetMapping
-    public ResponseEntity<String> index(){
+    public ResponseEntity<String> index() throws InterruptedException {
+        wait(3000);
         return ResponseEntity.ok("OK");
     }
 }
