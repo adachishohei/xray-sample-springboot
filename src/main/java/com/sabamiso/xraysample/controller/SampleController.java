4b9088e5b6675c5156ae894775c28595dde26dd4
adachishohei <dankan.0101@gmail.com>
2021-10-20 01:44:34 +0900

    modify

--- src/main/java/com/sabamiso/xraysample/controller/SampleController.java
+++ src/main/java/com/sabamiso/xraysample/controller/SampleController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
     @GetMapping
     public ResponseEntity<String> index() throws InterruptedException {
-        wait(3000);
+        Thread.sleep(3000);
         return ResponseEntity.ok("OK");
     }
 }
